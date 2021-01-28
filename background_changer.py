39f5d96a3fcf7b7a82ae4a005d671e88315d75eb
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2021-01-28 16:00:37 +0530

    Add files via upload

---
+++ background_changer.py
@@ -0,0 +1,43 @@
+# imports
+import cv2  
+import numpy as np  
+
+# initializing the videocapture and reading the image
+video = cv2.VideoCapture(0) 
+image = cv2.imread("bg.jfif") 
+  
+# while loop
+while True: 
+    # reading the video
+    ret, frame = video.read() 
+
+    # resizing the image
+    frame = cv2.resize(frame, (640, 480)) 
+    image = cv2.resize(image, (640, 480)) 
+  
+    # the color ranges
+    u_black = np.array([0, 0, 0]) 
+    l_black = np.array([0,0,30]) 
+    
+    # the mask using inrange of cv2
+    mask = cv2.inRange(frame, l_black, u_black) 
+    
+    # the result
+    res = cv2.bitwise_and(frame, frame, mask = mask) 
+    
+    # using the np.where fumction
+    f = frame - res 
+    f = np.where(f == 0, image, f) 
+    
+    # displaying the image in a window using imshow
+    cv2.imshow("video", frame) 
+    cv2.imshow("mask", f) 
+    
+    # breaking the while loop if escape or q is pressed
+    k = cv2.waitKey(1)
+    if k == 27 or k == ord('q'):
+        break 
+
+# destroying all the windows and closing the screens
+video.release() 
+cv2.destroyAllWindows() 
